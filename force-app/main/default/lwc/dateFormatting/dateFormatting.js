e2a79df63374bb9564044f5376dae54e190e851a
Humblefool27 <humblefool27@gmail.com>
2023-05-21 20:21:46 +0530

    general select commit

--- force-app/main/default/lwc/dateFormatting/dateFormatting.js
+++ force-app/main/default/lwc/dateFormatting/dateFormatting.js
@@ -6,6 +6,56 @@ const convertDate = dateFormat => {
     return date.getFullYear() + '-' + (m <= 9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
 };
 
+const tomsaDateFormat = dateFormat =>{
+    if(dateFormat){
+        let dte =(typeof(dateFormat)==='string')? new Date(dateFormat): dateFormat;
+        let strArray = [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+        ];
+        let d = dte.getDate();
+        let m = strArray[dte.getMonth()];
+        let y = dte.getFullYear();
+        return ''+(d<= 9?'0'+d:d)+'- '+m+'- '+y; 
+    }
+    else{
+        return null;
+    }
+}
+const tomsaDatetime = dateTime =>{
+    if(dateFormat){
+        let dte =(typeof(dateFormat)==='string')? new Date(dateFormat): dateFormat;
+        let strArray = [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+        ];
+        let d = dte.getDate();
+        let m = strArray[dte.getMonth()];
+        let y = dte.getFullYear();
+     
+}
+
 export {
-    convertDate
+    convertDate,
+    tomsaDateFormat
 };
